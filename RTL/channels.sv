4d7242fbd770c88e4d9073b43eb57e162d928a65
Krish <ksule14005@gmail.com>
2026-05-25 21:45:53 -0400

    started interfaces

---
+++ RTL/channels.sv
@@ -0,0 +1,7 @@
+interface tilelink_channels #(
+    parameter ADDR_WIDTH = 16,
+    parameter DATA_WIDTH = 128,
+    parameter SIZE_WIDTH = 3,
+    parameter SOURCE_WIDTH = 1,
+    parameter SINK_WIDTH = 1,
+);
